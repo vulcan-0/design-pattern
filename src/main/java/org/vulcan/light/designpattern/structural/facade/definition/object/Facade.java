e0b25533e1792e78aecbcc3baa33c12bb7c7936d
Sam Lu <luxiaocong524@163.com>
2021-05-25 08:42:25 +0800

    feat: add facade definition code

---
+++ src/main/java/org/vulcan/light/designpattern/structural/facade/definition/object/Facade.java
@@ -0,0 +1,14 @@
+package org.vulcan.light.designpattern.structural.facade.definition.object;
+
+/**
+ * @author Sam Lu
+ * @date 2021/5/25
+ */
+public interface Facade {
+
+    /**
+     * 操作
+     */
+    void operation();
+
+}
---
+++ src/main/java/org/vulcan/light/designpattern/structural/facade/definition/object/SubSystemB.java
@@ -0,0 +1,17 @@
+package org.vulcan.light.designpattern.structural.facade.definition.object;
+
+/**
+ * @author Sam Lu
+ * @date 2021/5/25
+ */
+public class SubSystemB {
+
+    public void operation3() {
+        System.out.println("SubSystemB operation3");
+    }
+
+    public void operation4() {
+        System.out.println("SubSystemB operation4");
+    }
+
+}
