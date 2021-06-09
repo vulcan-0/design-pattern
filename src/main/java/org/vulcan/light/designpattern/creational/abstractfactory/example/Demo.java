6bb44bc932cbfcfa19e5c97f9f92aab5273a0609
Sam Lu <luxiaocong524@163.com>
2021-06-09 08:40:46 +0800

    feat: update abstract factory example code

--- src/main/java/org/vulcan/light/designpattern/creational/abstractfactory/example/Demo.java
+++ src/main/java/org/vulcan/light/designpattern/creational/abstractfactory/example/Demo.java
@@ -1,8 +1,13 @@
 package org.vulcan.light.designpattern.creational.abstractfactory.example;
 
 /**
+ * 抽象工厂模式示例
+ *
  * @author Sam Lu
  * @date 2021/6/8
+ * @see javax.xml.parsers.DocumentBuilderFactory#newInstance()
+ * @see javax.xml.transform.TransformerFactory#newInstance()
+ * @see javax.xml.xpath.XPathFactory#newInstance()
  */
 public class Demo {
 
