1ec60ae5a20a71f955fa2302712f8c0542688949
Sam Lu <luxiaocong524@163.com>
2021-06-10 08:45:33 +0800

    feat: add builder example code

---
+++ src/main/java/org/vulcan/light/designpattern/creational/builder/example/CarType.java
@@ -0,0 +1,22 @@
+package org.vulcan.light.designpattern.creational.builder.example;
+
+/**
+ * @author Sam Lu
+ * @date 2021/6/9
+ */
+public enum CarType {
+
+    /**
+     * 微型车
+     */
+    CITY_CAR,
+    /**
+     * 跑车
+     */
+    SPORTS_CAR,
+    /**
+     * SUV
+     */
+    SUV
+
+}
